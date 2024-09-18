30d2ecd6458bdc2ef452e705d104bac959e515e1
TlucasX5 <sl93@sunyorange.edu>
2024-09-18 19:03:59 -0400

    Update Assign4

--- assignments/Assignment4.js
+++ assignments/Assignment4.js
@@ -2,6 +2,7 @@
 // #2 Return the largest of 3 arguments numeric or string p.112
 function max3(x, y, z) {
     if ((x > y) && (x > z)) {
+<<<<<<< Updated upstream
         return console.log(x + " is the largest");
     } else if ((y > x) && (y > z)) {
         return console.log(y + " is the largest");
@@ -13,12 +14,25 @@ function max3(x, y, z) {
 max3(30, 50, 20)
 max3("bite", "bat", "bit")
 max3(3.14, 3.141, -3.145)
+=======
+        return x;
+    } else if ((y > x) && (y > z)) {
+        return y;
+    } else {
+        return z;
+    }
+}
+console.log(max3(30, 50, 20));
+console.log(max3("bite", "bat", "bit"));
+console.log(max3(3.14, 3.141, -3.145));
+>>>>>>> Stashed changes
 
 
 
 // #5 Sum of odd integers p.112
 function sumOfOddIntegers(n) {
     let sum = 0;
+<<<<<<< Updated upstream
     let ctr = 1;
     // Start from 1 and increment by 2 fro odd numbers
     for(let i = 1; ctr <= n; i += 2) {
@@ -32,6 +46,19 @@ function sumOfOddIntegers(n) {
   console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(4));
   console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(5));
   console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(6));
+=======
+    n = (2 * n) - 1;
+    // Start from 1 and increment by 2 for odd numbers
+    for (let i = 1; i <= n; i += 2) {
+            sum = sum + i;
+    }
+    return sum;
+}
+    
+console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(4));
+console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(5));
+console.log("The sum of odd numbers for this integer is: " + sumOfOddIntegers(6));
+>>>>>>> Stashed changes
 
 
 // #9 rewrite "for" loop to work with "while" loop p.113
